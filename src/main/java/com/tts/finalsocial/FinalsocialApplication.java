2205f8c2f4a0720843f672bfa56075ad34dce368
Ricardo P <ricardopaucar1363@gmail.com>
2020-11-24 08:39:04 -0600

    init commit

---
+++ src/main/java/com/tts/finalsocial/FinalsocialApplication.java
@@ -0,0 +1,13 @@
+package com.tts.finalsocial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalsocialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalsocialApplication.class, args);
+	}
+
+}
